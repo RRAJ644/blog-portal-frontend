023b8e4c804ea86bdd0c6c2b816713d2425fd159
RRAJ644 <riturajsingh.17248@gmail.com>
2024-07-21 03:17:26 +0530

    content persistence

--- src/context/EditorContext.jsx
+++ src/context/EditorContext.jsx
@@ -1,12 +1,26 @@
-import { createContext, useState, useContext } from 'react'
+import { createContext, useState, useContext, useEffect } from 'react'
 
 const EditorContext = createContext()
 
 export const EditorProvider = ({ children }) => {
   const [editorContent, setEditorContent] = useState('')
 
+  useEffect(() => {
+    const savedContent = sessionStorage.getItem('editorContent')
+    if (savedContent) {
+      setEditorContent(savedContent)
+    }
+  }, [])
+
+  const handleSetEditorContent = (content) => {
+    setEditorContent(content)
+    sessionStorage.setItem('editorContent', content)
+  }
+
   return (
-    <EditorContext.Provider value={{ editorContent, setEditorContent }}>
+    <EditorContext.Provider
+      value={{ editorContent, setEditorContent: handleSetEditorContent }}
+    >
       {children}
     </EditorContext.Provider>
   )
