7f9095db443c193667e872be817411a60bdb8871
RRAJ644 <riturajsingh.17248@gmail.com>
2024-07-23 15:43:40 +0530

    editor module

--- src/pages/Write.jsx
+++ src/pages/Write.jsx
@@ -1,59 +1,112 @@
-// import React, { useState } from 'react'
-// import ReactQuill from 'react-quill'
-// import 'react-quill/dist/quill.snow.css' // Import styles
-
-// // Custom toolbar options
-// const toolbarOptions = {
-//   toolbar: [
-//     [{ 'font': ['sans-serif', 'serif', 'monospace'] }],
-//     [{ header: [1, 2, 3, 4, 5, 6, false] }],
-//     [{ list: 'ordered' }, { list: 'bullet' }],
-//     ['bold', 'italic', 'underline'],
-//     [{ align: [] }],
-//     ['link', 'image'],
-//     [{ color: [] }, { background: [] }],
-//     ['clean'],
-//   ],
-// }
-
-// const Write = () => {
-//   const [value, setValue] = useState('')
-
-//   return (
-//     <ReactQuill
-//       theme='snow'
-//       value={value}
-//       onChange={setValue}
-//       modules={toolbarOptions}
-//       placeholder='Write something amazing...'
-//     />
-//   )
-// }
-
-// export default Write
-
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import ReactQuill from 'react-quill'
 import 'react-quill/dist/quill.snow.css'
 import { useEditor } from '../context/EditorContext'
 import { Button } from '@mui/material'
 
-// Custom toolbar options
-const toolbarOptions = {
+const modules = {
   toolbar: [
-    [{ font: ['sans-serif', 'serif', 'monospace'] }],
-    [{ header: [1, 2, 3, 4, 5, 6, false] }],
-    [{ list: 'ordered' }, { list: 'bullet' }],
-    ['bold', 'italic', 'underline'],
-    [{ align: [] }],
-    ['link', 'image'],
+    [{ header: [1, 2, 3, false] }],
+    [{ font: [] }, { size: [] }],
+    ['bold', 'italic', 'underline', 'strike'],
     [{ color: [] }, { background: [] }],
+    [{ script: 'sub' }, { script: 'super' }],
+    ['blockquote', 'code-block'],
+    [{ list: 'ordered' }, { list: 'bullet' }],
+    [{ indent: '-1' }, { indent: '+1' }],
+    [{ direction: 'rtl' }],
+    [{ align: [] }],
+    ['link', 'image', 'video'],
     ['clean'],
   ],
 }
 
+const formats = [
+  'header',
+  'font',
+  'size',
+  'bold',
+  'italic',
+  'underline',
+  'strike',
+  'color',
+  'background',
+  'script',
+  'blockquote',
+  'code-block',
+  'list',
+  'bullet',
+  'indent',
+  'direction',
+  'align',
+  'link',
+  'image',
+  'video',
+]
+
 const Write = () => {
   const { editorContent, setEditorContent } = useEditor()
+  const [content, setContent] = useState('')
+
+  const handleSave = () => {
+    setContent(editorContent)
+  }
+
+  useEffect(() => {
+    const quillEditor = document.querySelector('.ql-editor')
+    if (quillEditor) {
+      quillEditor.classList.add(
+        'prose-customParagraph',
+        'max-lg:text-xl',
+        'max-sm:text-lg'
+      )
+      // Add custom styles for headings
+      const headings = quillEditor.querySelectorAll('h2')
+      headings.forEach((heading) => {
+        heading.classList.add(
+          'prose-customHeading',
+          'max-lg:text-center',
+          'max-lg:text-2xl',
+          'max-sm:text-xl'
+        )
+      })
+
+      const headings3 = quillEditor.querySelectorAll('h3')
+      headings3.forEach((heading) => {
+        heading.classList.add(
+          'prose-customHeading',
+          'max-lg:text-center',
+          'max-lg:text-2xl',
+          'max-sm:text-xl'
+        )
+      })
+
+      // Add custom styles for paragraphs
+      const paragraphs = quillEditor.querySelectorAll('p')
+      paragraphs.forEach((paragraph) => {
+        paragraph.classList.add(
+          'prose-customParagraph',
+          'max-lg:text-xl',
+          'max-sm:text-lg'
+        )
+      })
+      // Add custom styles for list items
+      const listItems = quillEditor.querySelectorAll('li')
+      listItems.forEach((li) => {
+        li.classList.add(
+          'prose-customParagraph',
+          'max-lg:text-xl',
+          'max-sm:text-lg'
+        )
+      })
+
+      //Add for image
+      const images = quillEditor.querySelectorAll('img')
+      images.forEach((img) => {
+        img.classList.add('mx-auto', 'block')
+      })
+    }
+  }, [editorContent])
 
   return (
     <div className='border-2'>
@@ -61,11 +114,12 @@ const Write = () => {
         theme='snow'
         value={editorContent}
         onChange={setEditorContent}
-        modules={toolbarOptions}
-        placeholder='Write something amazing...'
+        modules={modules}
+        formats={formats}
+        // className='prose'
       />
-
-      <Button>Save</Button>
+      <Button onClick={handleSave}>Save</Button>
+      {content.length > 0 && <div>{content}</div>}
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,55 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: ['./src/**/*.{html,js,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#E26433E8',
+        'header-bg': '#272727',
+        'heading-color': 'rgb(244, 240, 225)',
+        'social-color': '#F4F0E1',
+        'card-text': '#777777',
+        'tag-bg': '#69727d',
+        'link-color': '#471FF3',
+      },
+      typography: {
+        customHeading: {
+          css: {
+            color: '#E26433E8',
+            fontSize: '1.6rem',
+            fontWeight: '600',
+            textTransform: 'uppercase',
+            // paddingTop: '0.8em',
+            // paddingBottom: '0.8rem',
+          },
+        },
+        customParagraph: {
+          css: {
+            // color: '#000',
+            // color: '#E26433E8',
+            fontSize: '1.4rem',
+          },
+        },
+      },
+      border: {
+        borderCustom: '#F4F0E1', // custom border color
+      },
+      backgroundImage: {
+        'blogs-header': "url('/src/assets/blogs-header1.png')",
+      },
+      listStyleType: {
+        'custom-disc': 'disc',
+      },
+      spacing: {
+        'custom-pl': '1.25rem', // Adjust this value as needed
+      },
+      fontFamily: {
+        roboto: ['Roboto', 'sans-serif'],
+        poppins: ['Poppins', 'sans-serif'],
+      },
+    },
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/typography'), // Include the typography plugin
+  ],
 }
